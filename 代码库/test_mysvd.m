b7b8b18a0737c3942ce8d2dcd0e41a8e79a0e9fa
Isaac Tadokoro <28519848+RisingInIris2017@users.noreply.github.com>
2021-03-12 21:46:44 +0800

    Update test_mysvd.m

--- 代码库/test_mysvd.m
+++ 代码库/test_mysvd.m
@@ -8,10 +8,14 @@ for i=1:1000
    b=fix(200*rand)+2;%随机列数
    mat=rand(a,b);%随机矩阵
    [stdU,stdS,stdV]=svd(mat);%标准参考
-   [myU,myS,myV]=mysvd(mat);%自己的运算结果
+   
+   % 如需测试其他名称的 SVD 算法，修改这一行中的函数名即可。
+   % 这里以 mysvd() 函数为例。
+   [myU,myS,myV]=mysvd(mat);
+   
    errS=[errS,max(abs(myS-stdS))];%奇异值的差
    err=[err,max(max(abs(myU*myS*myV'-mat)))];%还原之后相比于原矩阵的差
 end
 %% 输出最大值
-max(errS)
-max(err)
+maxErrS=max(errS)
+maxErr=max(err)
