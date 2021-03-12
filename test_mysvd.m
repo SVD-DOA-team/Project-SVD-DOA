608d25a5000085c17f58dfb9353f0f022d20e9b3
Sheep-Jason <62644480+Sheep-Jason@users.noreply.github.com>
2021-03-12 20:31:36 +0800

    Update test_mysvd.m

--- test_mysvd.m
+++ test_mysvd.m
@@ -1,46 +1,17 @@
-% clc;clear;close all;
-% errU=[];
-% errS=[];
-% errV=[];
-% for i=1:1
-%    a=fix(20*rand)+2;
-%    b=fix(20*rand)+2;
-%    mat=rand(a,b);
-%    [stdU,stdS,stdV]=svd(mat);
-%    [myU,myS,myV]=mysvd(mat);
-% %    errU=[errU,max(abs(myU-stdU))];
-% %    errS=[errS,max(abs(myS-stdS))];
-% %    errV=[errV,max(abs(myV-stdV))];
-%     
-%    errU=max(max(abs(myU-stdU)));
-%    errS=max(max(abs(myS-stdS)));
-%    errV=max(max(abs(myV-stdV)));
-%    if max(errU,errS,errV)>1e-6
-%        pause(inf);
-%    end
-% end
-% % max(errU)
-% % max(errS)
-% % max(errV)
-
-
-
-
-
 clc;clear;close all;
-errU=[];
+%% 初始化保存运算误差的两个变量
 errS=[];
-errV=[];
+err=[];
+%% 循环检验算法的有效性
 for i=1:1000
-   a=fix(20*rand)+2;
-   b=fix(20*rand)+2;
-   mat=rand(a,b);
-   [stdU,stdS,stdV]=svd(mat);
-   [myU,myS,myV]=mysvd(mat);
-   errU=[errU,max(abs(myU-stdU))];
-   errS=[errS,max(abs(myS-stdS))];
-   errV=[errV,max(abs(myV-stdV))];
+   a=fix(200*rand)+2;%随机行数
+   b=fix(200*rand)+2;%随机列数
+   mat=rand(a,b);%随机矩阵
+   [stdU,stdS,stdV]=svd(mat);%标准参考
+   [myU,myS,myV]=mysvd(mat);%自己的运算结果
+   errS=[errS,max(abs(myS-stdS))];%奇异值的差
+   err=[err,max(max(abs(myU*myS*myV'-mat)))];%还原之后相比于原矩阵的差
 end
-errU
-errS
-errV
+%% 输出最大值
+max(errS)
+max(err)
