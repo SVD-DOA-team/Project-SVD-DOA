a4be1873d9b6f5d2772d84aa526f9d50c99f0b2d
Isaac Tadokoro <28519848+RisingInIris2017@users.noreply.github.com>
2021-03-13 09:05:49 +0800

    Update homemade_sorted_svd.m

--- 代码库/homemade_sorted_svd.m
+++ 代码库/homemade_sorted_svd.m
@@ -19,10 +19,15 @@ function [sortedEigVecU,singularValueMatrix,sortedEigVecV]=homemade_sorted_svd(m
     % 纠正左奇异矩阵的各列向量的符号
     for index=1:min(length(sortedEigVecU),length(sortedEigVecV))
     % index 的取值范围取决于左右奇异矩阵中尺寸较小的一个的尺寸
-        if (matrixToCompute*sortedEigVecV(:,index)+vectorOfEigVal(index)*sortedEigVecU(:,index))<1e-10
+    
+        % if (matrixToCompute*sortedEigVecV(:,index)+vectorOfEigVal(index)*sortedEigVecU(:,index))<1e-10
+        if (matrixToCompute*sortedEigVecV(:,index)+vectorOfEigVal(index)*sortedEigVecU(:,index))<1e-6
+        
         % 左右奇异矩阵的列向量必须满足 A*vi=sigma*ui 这一等量关系
         % 如果相差一个负号，必有 A*vi=-sigma*ui
         % 为了浮点数检验方便，将上式移项，改为检查 A*vi+sigma*ui<误差上限
+        % 此处将误差限从 1e-10 上调至 1e-6，有效地避免了因计算精度导致的偏差
+        
             sortedEigVecU(:,index)=-sortedEigVecU(:,index);
             % 若满足“相差一个负号”的条件，就将这一列列向量反号
         end
