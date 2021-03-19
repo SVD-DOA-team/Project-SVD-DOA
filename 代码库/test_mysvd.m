9a272d5e2c45d1f8219ee793a9846474f66f0167
Isaac Tadokoro <28519848+RisingInIris2017@users.noreply.github.com>
2021-03-19 11:14:11 +0800

    Update test_mysvd.m

--- 代码库/test_mysvd.m
+++ 代码库/test_mysvd.m
@@ -1,21 +1,23 @@
 clc;clear;close all;
+%% 可配置项，修改此处来决定可容忍的误差精度
+tolerableErr = 1e-8;
 %% 初始化保存运算误差的两个变量
 errS=[];
 err=[];
 %% 循环检验算法的有效性
-for i=1:1000
+for i=1:10000
    a=fix(200*rand)+2;%随机行数
    b=fix(200*rand)+2;%随机列数
-   mat=rand(a,b);%随机矩阵
+   mat=rand(a,b)+i*rand(a,b);%随机矩阵
    [stdU,stdS,stdV]=svd(mat);%标准参考
-   
-   % 如需测试其他名称的 SVD 算法，修改这一行中的函数名即可。
-   % 这里以 mysvd() 函数为例。
-   [myU,myS,myV]=mysvd(mat);
-   
+   [myU,myS,myV]=homemade_sorted_svd(mat);%自己的运算结果
    errS=[errS,max(abs(myS-stdS))];%奇异值的差
    err=[err,max(max(abs(myU*myS*myV'-mat)))];%还原之后相比于原矩阵的差
 end
 %% 输出最大值
 maxErrS=max(errS)
 maxErr=max(err)
+
+%% 输出算法出错的频率
+freqErrS=length(find(errS>tolerableErr))/length(errS)
+freqErr=length(find(err>tolerableErr))/length(err)
